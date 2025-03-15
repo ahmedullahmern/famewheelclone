a65cb3f4ed99dbea82ea8c88a5cf1d8742ef4fb4
ahmedullahmern <ahmedullahmern@gmail.com>
2025-03-15 23:29:13 +0500

    -

--- src/App.jsx
+++ src/App.jsx
@@ -156,9 +156,38 @@ function App() {
                 </h5>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                   {data.description}
-                  
+
                 </p>
-                  
+
+              </div>
+            </a>
+          ))
+        }
+
+      </div>
+
+      // logo cards
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-20 px-10">
+        {
+          cardsData.map((data) => (
+            <a
+              href="#"
+              className="flex flex-col sm:flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+            >
+              <img
+                className="sm:w-[33.33333333%] sm:h-10 object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                src={data.image}
+                alt=""
+              />
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {data.title}
+                </h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  {data.description}
+
+                </p>
+
               </div>
             </a>
           ))
