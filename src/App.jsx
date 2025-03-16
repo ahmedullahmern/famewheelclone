0b8579eb8f26db549044722b622af1dccca82832
ahmedullahmern <ahmedullahmern@gmail.com>
2025-03-16 23:15:57 +0500

    add logo cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,21 @@ import carwithremote from './assets/carwithremote.png'
 import inspection from './assets/carinspaction.png'
 import insurance from './assets/carinsurance.png'
 import auction from './assets/sellbilding.png'
+import carfinance from './assets/carfinance.png'
+import jetour from './assets/jetourlogo.png'
+import toyota from './assets/toyotalodo.png'
+import suzuki from './assets/suzukilogo.png'
+import honda from './assets/hundalogo.png'
+import daihatsu from './assets/daihatsulogo.png'
+import kia from './assets/kialogo.png'
+import peugeot from './assets/peugologo.png'
+import hyundai from './assets/hyundai.png'
+import haval from './assets/havallogo.png'
+import changan from './assets/changelogo.png'
+import dfsk from './assets/dfsklogo.png'
+import mg from './assets/mglogo.png'
+import audi from './assets/audilogo.png'
+import faw from './assets/fawlogo.png'
 import './App.css'
 
 const cardsData = [
@@ -48,10 +63,27 @@ const cardsData = [
     id: 6,
     title: "Car Finance",
     description: "FameWheels proudly offers premier Financing services in Pakistan.",
-    image: auction,
+    image: carfinance,
     link: "#",
   },
 
+];
+
+const cardsLogoData = [
+  { image: jetour, title: "Jetour" },
+  { image: toyota, title: "Toyota" },
+  { image: suzuki, title: "Suzuki" },
+  { image: honda, title: "Honda" },
+  { image: daihatsu, title: "Daihatsu" },
+  { image: kia, title: "KIA" },
+  { image: peugeot, title: "Peugeot" },
+  { image: hyundai, title: "Hyundai" },
+  { image: haval, title: "Haval" },
+  { image: changan, title: "Changan" },
+  { image: dfsk, title: "DFSK" },
+  { image: mg, title: "MG" },
+  { image: audi, title: "Audi" },
+  { image: faw, title: "FAW" },
 ];
 
 
@@ -59,6 +91,8 @@ function App() {
 
   return (
     <>
+
+      {/* top header */}
       <div style={{ backgroundImage: `url(${karachi})` }} className="relative h-[530px] bg-cover bg-center" >
 
         <div className="absolute inset-0 bg-gradient-to-b from-black/75 via-transparent to-black/75"></div>
@@ -71,6 +105,7 @@ function App() {
           <button>Login</button>
         </div>
 
+        {/* tarnsparent header */}
 
         <div className="mt-12 ml-20 fixed top-0 left-0 w-[90%] bg-white/20 backdrop-blur-md z-50 p-5 rounded-[50px_50px_0_50px]">
           <div className="flex justify-between items-center max-w-7xl mx-auto">
@@ -95,6 +130,8 @@ function App() {
           </div>
         </div>
 
+        {/* hero section */}
+
         <div className="relative z-10 flex justify-center items-center mt-40">
           <div className="">
             <h1 className='text-5xl font-bold text-white'>Discover, Buy & Sell Cars in Pakistan</h1>
@@ -104,7 +141,7 @@ function App() {
           </div>
         </div>
 
-
+        {/* search box */}
 
         <div className="flex justify-center items-center mt-44">
           {/* Search Box Container */}
@@ -151,7 +188,7 @@ function App() {
                 alt=""
               />
               <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <h5 className="mb-2 text-[20px] font-bold tracking-tight text-gray-900 dark:text-white">
                   {data.title}
                 </h5>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
@@ -166,34 +203,19 @@ function App() {
 
       </div>
 
-      // logo cards
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-20 px-10">
-        {
-          cardsData.map((data) => (
-            <a
-              href="#"
-              className="flex flex-col sm:flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-            >
-              <img
-                className="sm:w-[33.33333333%] sm:h-10 object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                src={data.image}
-                alt=""
-              />
-              <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {data.title}
-                </h5>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {data.description}
+      {/* // logo cards */}
 
-                </p>
-
-              </div>
-            </a>
-          ))
-        }
-
+      <h1 className='flex justify-start font-bold text-3xl p-5 m-10 0 5 5 '>Browse Cars by Make</h1>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-6 mt-10 px-10">
+        {cardsLogoData.map((data, index) => (
+          <div key={index} className="bg-white shadow-md rounded-lg flex flex-col items-center p-4">
+            <img className="w-20 h-20 object-contain" src={data.image} alt={data.title} />
+            <p className="mt-2 text-gray-700 font-medium text-center">{data.title}</p>
+          </div>
+        ))}
       </div>
+
+      
     </>
 
   )
